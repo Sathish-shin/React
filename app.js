c400c7e3f3c923454b95e7fd095658126150ba11
Sathish <pagdalasathish@gmail.com>
2025-05-30 14:52:41 +0530

    Files added

---
+++ app.js
@@ -0,0 +1,22 @@
+// const root = document.getElementById('root');
+// const heading = document.createElement('h1')
+// heading.innerHTML = "Hello  from React"
+// root.appendChild(heading)
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ])]
+);
+
+// const heading = React.createElement("h1", {id : "heading"}, "Hello from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+// console.log(root);
